bdd6fac4f666d2b9edf7b9878e6ef4eeff15ef6d
remake <verticle@foxmail.com>
2022-05-19 16:27:24 +0800

    feat: update dns server api

---
+++ lib/dns/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod buffer;
+pub mod result;
+pub mod header;
+pub mod query_type;
+pub mod packet;
+pub mod question;
+pub mod record;
---
+++ lib/dns/src/question.rs
@@ -0,0 +1,35 @@
+use crate::buffer::BytePacketBuffer;
+use crate::query_type::QueryType;
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DnsQuestion {
+    pub name: String,
+    pub qtype: QueryType,
+}
+
+impl DnsQuestion {
+    pub fn new(name: String, qtype: QueryType) -> DnsQuestion {
+        DnsQuestion { name, qtype }
+    }
+
+    pub fn read(&mut self, buffer: &mut BytePacketBuffer) -> Result<()> {
+        buffer.read_qname(&mut self.name)?;
+        self.qtype = QueryType::from_num(buffer.read_u16()?); // qtype
+        let _ = buffer.read_u16()?; // class
+
+        Ok(())
+    }
+
+    pub fn write(&self, buffer: &mut BytePacketBuffer) -> Result<()> {
+        buffer.write_qname(&self.name)?;
+
+        let typenum = self.qtype.to_num();
+        buffer.write_u16(typenum)?;
+        buffer.write_u16(1)?;
+
+        Ok(())
+    }
+}
